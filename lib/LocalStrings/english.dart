b7b450c2b9823533f7dda904c03ae99e617d08ee
praveenreddy9 <vangalapraveen.vpr@gmail.com>
2023-10-25 15:48:56 +0530

    :after lang setup through index flow

---
+++ lib/LocalStrings/english.dart
@@ -0,0 +1,8 @@
+class englishLang {
+  var keys = {
+    'hello': 'Hello World',
+    'message': 'Welcome to office today',
+    'title': 'Flutter Language - Localization',
+    'changelang': 'Change Language'
+  };
+}
---
+++ lib/LocalStrings/kannada.dart
@@ -0,0 +1,9 @@
+class kannadaLang {
+  var keys = {
+    'hello': 'ಹಲೋ ವರ್ಲ್ಡ್',
+    'message': 'ಪ್ರೋಟೋ ಕೋಡರ್ ಪಾಯಿಂಟ್‌ಗೆ ಸುಸ್ವಾಗತ',
+    'title': 'ಬೀಸು ಭಾಷೆ - ಸ್ಥಳೀಕರಣ',
+    'subscribe': 'ವಂತಿಗೆ ಕೊಡು',
+    'changelang': 'ಭಾಷೆ ಬದಲಿಸಿ'
+  };
+}
